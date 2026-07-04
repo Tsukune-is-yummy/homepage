46906860031bd2bf96768beb5b97018be1a9dd3f
Tsukune-is-yummy <ryochin.dayo0302@gmail.com>
2026-07-05 00:03:31 +0900

    V1.3

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="w-full bg-black">
+    <div className="flex flex-col min-h-screen bg-black text-white">
+      <main className="w-full flex-1 pb-24">
         <Link href="/" className="block w-full overflow-hidden p-3">
           <Image
             loading="eager"
@@ -17,20 +17,20 @@ export default function Home() {
             className="w-full h-auto object-cover max-w-none"
           />
         </Link>
-        <div className="mx-auto max-w-250 max-h-50 my-5 flex justify-center rounded-lg border border-zinc-700 bg-zinc-800/50 p-1 shadow-xl">
+        <div className="mx-auto max-w-4xl max-h-50 my-5 flex justify-center rounded-lg border border-zinc-700 bg-zinc-800/50 py-10 px-6 shadow-xl">
           <div className="min-h-100 text-gray-300">
-            <p className="mt-4 text-3xl text-white font-mochiy">
+            <p className="text-xl md:text-3xl font-mochiy leading-relaxed">
               つくねのサーバーへようこそ！
             </p>
-            <p className="font-mochiy mt-4 text-3xl text-white">
+            <p className="text-xl md:text-3xl font-mochiy leading-relaxed">
               ここは 管理者：つくね が運営しているつくねサーバーです！
             </p>
           </div>
         </div>
-        <p className="font-sans flex justify-start text-2xl font-bold tracking-widest text-white mx-auto mt-10 max-w-250">
+        <p className="font-sans flex justify-start text-2xl font-bold tracking-widest text-white mx-auto mt-10 max-w-5xl">
           INDEX
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6 max-w-5xl mx-auto px-4 w-full">
           <Link
             href="/blog"
             className="block p-5 rounded-lg border border-zinc-800 bg-zinc-900/50 hover:bg-zinc-700 hover:border-zinc-900/80 transition-all duration-300 ease-in-out"
